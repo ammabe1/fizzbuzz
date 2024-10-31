9e0f75a26f309d3f7492d81eb3b6be20c86fed8d
Amy Mabe <amfautz@gmail.com>
2024-10-31 16:31:10 -0400

    corrected code

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,9 +1,11 @@
 for i in range(1, 101):
   print(i, end=" ")
-  if i % 3 == 0 and i % 5 == 0:
+  if (i % 3 == 0) and (i % 5 == 0):
     print("fizzbuzz")
-  elif i % 3 == 0:
+  elif (i % 3 == 0):
     print("fizz")
-  elif i % 5 == 0:
+  elif (i % 5 == 0):
     print("buzz")
+  else: 
+    print(i)
 
