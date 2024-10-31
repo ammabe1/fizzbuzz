d479942eb41d9181433f03f8750d5eab90f10c0f
Amy Mabe <amfautz@gmail.com>
2024-10-31 16:21:42 -0400

    fizzbuzz activity

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,9 @@
-# add your code here
+for i in range(1, 101):
+  print(i, end=" ")
+  if i % 3 == 0 and i % 5 == 0:
+    print("fizzbuzz")
+  elif i % 3 == 0:
+    print("fizz")
+  elif i % 5 == 0:
+    print("buzz")
 
